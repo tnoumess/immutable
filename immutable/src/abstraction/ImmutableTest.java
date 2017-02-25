0a5274abed32971ca42eadecbddc92912bcba6ab
tnoumessi <tnoumessi@gmail.com>
2017-02-24 19:24:17 -0500

    test10

--- immutable/src/abstraction/ImmutableTest.java
+++ immutable/src/abstraction/ImmutableTest.java
@@ -66,4 +66,13 @@ public class ImmutableTest {
 		assertEquals("3",l.choose());// will fail because choose
 	}
 
+       //test choose() of immutable stack again
+        @Test
+        public void testChoose_ImmutableStack2() {
+                l=l.put("4");
+                l=l.put("3");
+                l=l.put("9");
+                assertEquals("3",l.choose());// will fail because choose
+        }
+
 }
